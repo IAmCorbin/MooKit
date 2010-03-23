d58a9def5a48eff1678afdb05c91e54f1d46eb7a
Corbin Tarrant <Corbin@IAmCorbin.net>
2010-03-22 20:04:02 -0400

    Simplify magicMySQL function

--- php/functions.php
+++ php/functions.php
@@ -3,12 +3,8 @@
  * Check for Magic Quotes and Strip if on and return mysql_real_escape_string
  */
 function magicMySQL($var) {
-	if(get_magic_quotes_gpc())  {
-		$var = stripslashes($var);
-		return mysql_real_escape_string($var);
-	} else {
-		return mysql_real_escape_string($var);
-	}
+	if(get_magic_quotes_gpc()) $var = stripslashes($var);
+	return mysql_real_escape_string($var);
 }
 
 ?>
