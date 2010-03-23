d34a9252e031a16575962d530149b6279382b787
Corbin Tarrant <Corbin@IAmCorbin.net>
2010-03-23 13:25:55 -0400

    Fixed magicMySQL function to use proper mysqli style escaping

--- php/functions.php
+++ php/functions.php
@@ -2,9 +2,9 @@
 /**
  * Check for Magic Quotes and Strip if on and return mysql_real_escape_string
  */
-function magicMySQL($var) {
+function magicMySQL($DB,$var) {
 	if(get_magic_quotes_gpc()) $var = stripslashes($var);
-	return mysql_real_escape_string($var);
+	return mysqli_real_escape_string($DB,$var);
 }
 
 ?>
--- php/postUpdate.php
+++ php/postUpdate.php
@@ -1,17 +1,18 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'].'MooKit/php/includes.php'; INIT();
+require_once $_SERVER['DOCUMENT_ROOT'].'/MooKit/php/includes.php'; INIT();
 	//Create Post Object
 	$post = new Post($_POST['title'],$_POST['text'],null,date('Y-m-d H:i:s'));
 	//if no errors
 	if($post->errorStatus()) //FILTER FAILTURE
 		echo json_encode(array('status'=>'ERROR_FILTER'));
 	else { //NO ERRORS
-		//prepare vars for query
-		$titleMysql = magicMySQL($post->title);
-		$htmlMysql = magicMySQL($post->html);
 		//UPDATE POST IN Database
 		$DB = new DatabaseConnection;
-		if($_POST['post_id']) { //do not update if not a valid post number;
+		//prepare vars for query
+		$titleMysql = magicMySQL($DB->getLink(),$post->title);
+		$htmlMysql = magicMySQL($DB->getLink(),$post->html);
+		//do not update if not a valid post number;
+		if($_POST['post_id']) { 
 			if($DB->query("UPDATE `posts` SET title='".$titleMysql."', html='".$htmlMysql."', modTime=NOW() WHERE `post_id`='".$_POST['post_id']."';",null)) {
 				//SUCCESS
 				echo json_encode(array('status'=>'OK','title'=>stripslashes($post->title),'html'=>stripslashes($post->html)));
