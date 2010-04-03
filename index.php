5ebfcc68b869434d52e26fa897d205b5ac975501
Corbin Tarrant <Corbin@IAmCorbin.net>
2010-04-02 21:42:10 -0400

    Added .htaccess mod_rewrite rules and index.php routing

--- index.php
+++ index.php
@@ -1,38 +1,48 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'].'/MooKit/CodeCore/php/includes.php'; INIT(false);
 
-//Create new application
-$Demo = new MooKit;
+switch($_GET['request']) {
+	case 'blank/page':
+		break;
+	case '':
+		//Create new application
+		$Demo = new MooKit;
 
-//set title
-$Demo->main->title = "Demo MooKit Version 0.7 Application"; 	
+		//set title
+		$Demo->main->title = "Demo MooKit Version 0.7 Application"; 	
 
-//grab sub templates
-//$Demo->main->navTpl = new Template('templates/nav.tpl.php');
-	//create Main Menu
-	$mainMenu = new Menu;
-	$mainMenu->add('IAmCorbin.net','http://www.iamcorbin.net');
-	  $mainMenu->addSub('Skip Intro','http://www.iamcorbin.net/?intro=1');
-	  $mainMenu->addSub('The Desert','http://www.iamcorbin.net/desert');
-	$mainMenu->add('MetalDisco.org','http://www.metaldisco.org');
-	$mainMenu->add('Testing Ajax Links','');
-	  $mainMenu->addSub('test1','test1','ajaxLink');
-	  $mainMenu->addSub('test2','test2','ajaxLink');
-	  $mainMenu->addSub('test3','test3','ajaxLink');
-	//set mainmenu to template
-	$Demo->main->Menu = $mainMenu;	
-	
-//Login Form							
-$Demo->main->loginTpl = new Template('templates/loginForm.tpl.php'); 		/*add login form */		
-//Signup Form
-$Demo->main->signupTpl = new Template('templates/signupForm.tpl.php');	/* add signup form */	
+		//grab sub templates
+		//$Demo->main->navTpl = new Template('templates/nav.tpl.php');
+			//create Main Menu
+			$mainMenu = new Menu;
+			$mainMenu->add('IAmCorbin.net','http://www.iamcorbin.net');
+			  $mainMenu->addSub('Skip Intro','http://www.iamcorbin.net/?intro=1');
+			  $mainMenu->addSub('The Desert','http://www.iamcorbin.net/desert');
+			$mainMenu->add('MetalDisco.org','http://www.metaldisco.org');
+			$mainMenu->add('Testing Ajax Links','');
+			  $mainMenu->addSub('test1','test1','ajaxLink');
+			  $mainMenu->addSub('test2','test2','ajaxLink');
+			  $mainMenu->addSub('test3','test3','ajaxLink');
+			//set mainmenu to template
+			$Demo->main->Menu = $mainMenu;	
+			
+		//Login Form							
+		$Demo->main->loginTpl = new Template('templates/loginForm.tpl.php'); 		/*add login form */		
+		//Signup Form
+		$Demo->main->signupTpl = new Template('templates/signupForm.tpl.php');	/* add signup form */	
 
-//Content Area
-$Demo->main->contentTpl = getAuthContent(FALSE);
-$Demo->main->contentTpl->userIP = $_SESSION['ip'];
+		//Content Area
+		$Demo->main->contentTpl = getAuthContent(FALSE);
+		$Demo->main->contentTpl->userIP = $_SESSION['ip'];
 
-//OUTPUT
-$Demo->RUN();
+		//OUTPUT
+		$Demo->RUN();
+		
+		break;
+	default:
+		include '404.php';
+		break;
+}
 
 
 ?>
--- templates/main.tpl.php
+++ templates/main.tpl.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
+	<base href="http://10.10.10.100/MooKit/" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	
 	<title><?=$title ?></title>
@@ -14,7 +15,7 @@
 	<div id="mainNav">
 		<?= $Menu->output(); //$navTpl  // NAVIGATION BAR // ?>
 	</div>
-	
+
 	<?= $loginTpl // LOGIN FORM // ?>
 	<?= $signupTpl // SIGNUP FORM //?>
 	
