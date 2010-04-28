962d278e129fa9ab0b215fdd4ee94e0e45e8c1ca
Corbin Tarrant <Corbin@IAmCorbin.net>
2010-04-28 10:03:09 -0400

    Added GNU GENERAL PUBLIC LICENSE Version 3

--- public/templates/main.tpl.php
+++ public/templates/main.tpl.php
@@ -1,6 +1,20 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
+	<!--  
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	-->
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	
 	<title><?=$title ?></title>
@@ -8,7 +22,6 @@
 	<? if(isset($styles)) foreach($styles as $style) echo $style."\n\t"; ?>
 
 	<? if(isset($scripts)) foreach($scripts as $script) echo $script."\n\t"; ?>
-	
 </head>
 <body>
 	<? echo "<div class=\"userInfo\"><div>".(isset($_SESSION['alias'])? "Welcome ".$_SESSION['alias']." (".getHumanAccess($_SESSION['access_level']).")" : "Welcome Guest - Please Sign Up or Log In to access more features")
