b47b5c9846a00985131e118ce49e13902004db81
Corbin Tarrant <Corbin@IAmCorbin.net>
2010-05-07 02:57:42 -0400

    Fix Zebra Hover

--- public/style/secure/createPanel.css.php
+++ public/style/secure/createPanel.css.php
@@ -2,6 +2,9 @@
 
 .createTitle { font-family: Monospace; font-weight: bold; font-size: 30px; border-bottom: 4px groove #AAA; margin-bottom: 5px; }
 
+<? /* <!-- FIX ZEBRA HOVER COLORS --> */ ?>
+#posts tbody tr.alt:hover { background-color: #FAA; }
+
 <? /*<!-- DELETE POST BUTTON --> */?>
 td.createDeletePost { font-family: Monospace; font-weight: bold; text-align: center; font-size: 30px; }
 td.createDeletePost:hover { font-weight: bold; font-size: 30px; color: #500; }
