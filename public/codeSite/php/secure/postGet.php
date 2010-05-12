da36a56a829dead9f6fbf15284ff62517e0b79ca
Corbin Tarrant <Corbin@IAmCorbin.net>
2010-05-12 01:40:16 -0400

    Removed Unneeded files and updated Documentation

--- public/codeSite/php/secure/postGet.php
+++
@@ -1,9 +0,0 @@
-<?php
-//open connection	
-$DB = new DatabaseConnection;
-//get post and echo the json return
-if($post = $DB->get_row("SELECT `post_id`,`title`,`html` FROM `posts` WHERE `post_id`='".$_POST['post_id']."';","json"))
-	echo $post;
-else
-	echo "Error Retrieving Desired Post...Sorry, please try again later or contact the administrator";
-?>
--- public/style/secure/postEdit.css.php
+++
@@ -1,6 +0,0 @@
-<?php header("Content-type: text/css"); ?>
-/* POST CSS */
-
-.displayPost { display: relative; padding: 20px; overflow: hidden; width: 50%; height: 50%; }
-#renderTitle { font-weight: bold; font-size: 30px; border-bottom: solid black 3px; }
-#renderText { width: 100%; height: 100%; position: relative; }
